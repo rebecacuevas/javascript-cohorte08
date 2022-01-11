5578934634b04787373d201dbdb37c0453788495
Rebeca Cuevas <rebecaacuevasa@gmail.com>
2022-01-11 17:33:58 -0600

    age calculator

---
+++ advanced-js/data-types-var/exercise.js
@@ -0,0 +1,15 @@
+let currentYear = 2022;
+
+console.log(currentYear);
+
+function age() {
+    let yourYear = window.prompt();
+    let yourAge = currentYear - yourYear;
+    console.log(yourAge);
+    alert("Tienes " + yourAge + " años.");
+}
+
+
+
+// pedir año de nacimiento 
+// calcular edad
